5fab4bf89f8d869cf7ea78636b6fee033a1518bd
unknown <xindufresne@163.com>
2015-05-13 21:42:26 +0800

    Initial commit

---
+++ app/src/main/java/com/example/dt/jumpforwardjava/MainActivityTarget.java
@@ -0,0 +1,22 @@
+package com.example.dt.jumpforwardjava;
+
+/**
+ * Created by DT on 2015/5/13.
+ */
+ import android.support.v7.app.ActionBarActivity;
+ import android.os.Bundle;
+ import android.view.Menu;
+ import android.view.MenuItem;
+
+
+public class MainActivityTarget extends ActionBarActivity {
+
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+super.onCreate(savedInstanceState);
+setContentView(R.layout.activity_main_target);
+}
+
+
+
+}
